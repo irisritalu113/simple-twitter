705b6fded0d011bc739452975a17433687d8b3e8
Rita <rita.lu@yahoo.com.tw>
2018-02-07 23:54:22 +0800

    在admin/tweets_controller.rb中定義私有方法set_tweet

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,9 @@
 class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_tweet, only:  [:show, :destroy]
+
+
   def index
     @tweets = Tweet.all
   end
@@ -20,16 +23,19 @@ class Admin::TweetsController < Admin::BaseController
     end
   end
 
-  def show
-    @tweet = Tweet.find(params[:id])
-  end
-
   def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "This tweet was deleted"
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:description)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
